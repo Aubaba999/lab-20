8227607ae3b5feb7f531862269db5ed67f357543
Aubaba999 <sutjii1989@gmail.com>
2025-02-18 13:42:12 +0700

    20.1

--- lab20.h
+++ lab20.h
@@ -1,3 +1,4 @@
+
 #include<iostream>
 #include<string>
 #include<ctime>
@@ -15,22 +16,6 @@ class Equipment{
 		Equipment(int,int,int);
 		vector<int> getStat();			
 };
-
-Equipment::Equipment(int hp, int atke, int defe){
-	hpmax = hp;
-	atk = atke;
-	def = defe;
-}
-
-
-vector<int>Equipment::getStat(){
-	vector<int> temp;
-	temp.push_back(hpmax);
-	temp.push_back(atk);
-	temp.push_back(def);
-
-	return temp;
-}
 
 class Unit{
 		string name;
@@ -70,37 +55,12 @@ Unit::Unit(string t,string n){
 	}
 	hp = hpmax;	
 	guard_on = false;
+	dodge_on =false;
 	equipment = NULL;
 }
-//my code below this line
-
-int Unit::ultimateAttack(Unit & A){
-	return A.beAttacked(2*atk);
+int Unit::ultimateAttack(Unit &opp){
+	return opp.beAttacked(2*atk);
 }
-
-void Unit::dodge(){
-	dodge_on = true;
-} 
-
-void Unit::equip(Equipment * E){
-
-		if(equipment == NULL){
-			equipment = E;
-		}else{
-			hpmax -= equipment->getStat()[0];
-			atk -= equipment->getStat()[1];
-			def -= equipment->getStat()[2];
-		}
-		hpmax += (*E).getStat()[0];
-		atk += (*E).getStat()[1];
-		def += (*E).getStat()[2];
-		if(hp > hpmax){
-			hp -= (*equipment).getStat()[0];
-		}
-		equipment = E;
-}
-//end of my code
-
 void Unit::showStatus(){
 	if(type == "Hero"){
 		cout << "---------------------------------------\n"; 
@@ -116,36 +76,30 @@ void Unit::showStatus(){
 	}
 }
 
+void Unit:: dodge(){
+    dodge_on=true;
+}
+
 void Unit::newTurn(){
-	guard_on = false; 
-	dodge_on = false;
+	guard_on = false;
+	dodge_on =false;
 }
 
 int Unit::beAttacked(int oppatk){
 	int dmg;
-	
-	
 	if(oppatk > def){
 		dmg = oppatk-def;	
 		if(guard_on) dmg = dmg/3;
+		if(dodge_on){
+		    int w=rand()%2;
+		    if(w==0)dmg=0;
+		    else dmg=dmg*2;
 	}	
+	}
 	hp -= dmg;
 	if(hp <= 0){hp = 0;}
 	
-
-	int randomness = rand()%2;
-	if(dodge_on){
-		if(randomness == 0){
-			return 0;
-		}else{
-			hp = hp - (dmg*2);
-			return (dmg*2);
-		}
-	}
-
 	return dmg;	
-
-
 }
 
 int Unit::attack(Unit &opp){
@@ -227,3 +181,4 @@ void playerLose(){
 	cout << "*                                                     *\n";
 	cout << "*******************************************************\n";
 };
+
